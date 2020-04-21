17b93dfaaa4fdc960c996fb0bcbefd2a09829835
JulianCedric <julian.pormentilla@gmail.com>
2020-04-21 16:21:03 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+    def name=(name)
+        # setter method 
+        @name = name 
+    end
+
+    def name
+        # getter method
+        @name
+    end 
+
+    def breed=(breed)
+        # setter method 
+        @breed = breed 
+    end
+
+    def breed
+        # getter method
+        @breed
+    end 
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+    def name=(name)
+        # setter method 
+        @name = name 
+    end
+
+    def name
+        # getter method
+        @name
+    end 
+
+    def job=(job)
+        # setter method 
+        @job = job 
+    end
+
+    def job
+        # getter method
+        @job
+    end 
+
+end 
